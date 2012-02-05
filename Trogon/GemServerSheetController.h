628b8bd5fd0a788d09416b3de6291f0e04fab5d5
Ricky Nelson <socketwiz@Rickys-iMac.local>
2012-02-05 14:53:21 -0500

    Implemented the gem server sheet

--- Trogon/GemServerSheetController.h
+++ Trogon/GemServerSheetController.h
@@ -8,6 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
-@interface GemServerSheetController : NSObject
+@interface GemServerSheetController : NSObject {
+    NSView *documentWindow;
+    NSPanel *objectSheet;
+    NSMutableString *_action;
+    NSString *_port;
+}
+@property (nonatomic, retain) IBOutlet NSView *documentWindow;
+@property (nonatomic, retain) IBOutlet NSPanel *objectSheet;
 
+- (IBAction)add:(id)sender ruby:(NSString *)aRuby gem:(NSString *)aGem;
+- (IBAction)cancel:(id)sender;
+- (IBAction)complete:(id)sender;
+- (IBAction)startGemServer:(id)sender;
+- (IBAction)stopGemServer:(id)sender;
+- (IBAction)viewDocs:(id)sender;
+
+@property (weak) IBOutlet NSTextField *lblGem;
+@property (weak) IBOutlet NSTextField *txtPort;
+@property (unsafe_unretained) IBOutlet NSTextView *txtViewGemServerOutput;
+
+@property (weak) IBOutlet NSButton *btnStart;
+@property (weak) IBOutlet NSButton *btnStop;
+@property (weak) IBOutlet NSButton *btnClose;
+@property (weak) IBOutlet NSButton *btnViewDocs;
 @end
--- Trogon/GemServerSheetController.m
+++ Trogon/GemServerSheetController.m
@@ -9,5 +9,147 @@
 #import "GemServerSheetController.h"
 
 @implementation GemServerSheetController
+@synthesize lblGem;
+@synthesize txtPort;
+@synthesize txtViewGemServerOutput;
+@synthesize btnStart;
+@synthesize btnStop;
+@synthesize btnClose;
+@synthesize btnViewDocs;
+@synthesize documentWindow;
+@synthesize objectSheet;
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _action = [[NSMutableString alloc] init];
+    }
+    return self;
+}
+
+- (IBAction)add:(id)sender ruby:(NSString *)aRuby gem:(NSString *)aGem {
+    if (objectSheet == nil) {
+        NSBundle *myBundle = [NSBundle bundleForClass:[self class]];
+        NSNib *nib = [[NSNib alloc] initWithNibNamed:@"GemServerSheet" bundle:myBundle];
+        
+        BOOL success = [nib instantiateNibWithOwner:self topLevelObjects:nil];
+        if (success != YES) {
+            NSError *error;
+            NSMutableDictionary *errorDetail = [NSMutableDictionary dictionary];
+            [errorDetail setValue:@"Unable to load GemServerSheet.xib" forKey:NSLocalizedDescriptionKey];
+            error = [NSError errorWithDomain:@"TrogonGemServerSheetDomainError" code:100 userInfo:errorDetail];
+            [NSApp presentError:error];
+            NSLog(@"%@", [errorDetail valueForKey:NSLocalizedDescriptionKey]);
+            
+            return;
+        }
+    }
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(readGemServerOutput:)
+                                                 name:NSFileHandleDataAvailableNotification 
+                                               object:nil];
+    
+    [self.txtViewGemServerOutput setEditable:YES];
+    [self.txtViewGemServerOutput setString:@""];
+    [self.txtViewGemServerOutput setEditable:NO];
+    
+    NSString *gemLabel = [NSString stringWithFormat:@"%@@%@", aRuby, aGem];
+    [self.lblGem setStringValue:gemLabel];
+    
+    [btnStop setEnabled:NO];
+    [btnViewDocs setEnabled:NO];
+    
+    [NSApp beginSheet:objectSheet
+       modalForWindow:[documentWindow window]
+        modalDelegate:self
+       didEndSelector:@selector(objectSheetDidEnd:returnCode:contextInfo:)
+          contextInfo:nil];
+}
+
+- (IBAction)cancel:(id)sender {
+    [NSApp endSheet:objectSheet returnCode:NSCancelButton];
+}
+
+- (IBAction)complete:(id)sender {
+    [NSApp endSheet:objectSheet returnCode:NSOKButton];
+}
+
+- (IBAction)startGemServer:(id)sender {
+    [btnStart setEnabled:NO];
+    [btnStop setEnabled:YES];
+    [btnViewDocs setEnabled:YES];
+    [btnClose setEnabled:NO];
+    [txtPort setEditable:NO];
+    
+    _port = [NSString stringWithFormat:@"%i", 8808];
+    if ([[txtPort stringValue] localizedCompare:@""] != NSOrderedSame) {
+        _port = [txtPort stringValue];
+    }
+
+    NSString *serverText = [NSString stringWithFormat:@"Starting Gem Server on port: %@\n", _port];
+    [self.txtViewGemServerOutput setEditable:YES];
+    [self.txtViewGemServerOutput insertText:serverText];
+    [self.txtViewGemServerOutput setEditable:NO];
+
+    NSDictionary *data = [NSDictionary dictionaryWithObjectsAndKeys:_port, @"port", nil ];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"TrogonStartGemServer" 
+                                                        object:self
+                                                      userInfo:data];
+}
+
+-(void)readGemServerOutput: (NSNotification *)notification {
+    NSData *data;
+    NSString *text;
+    
+    data = [[notification object] availableData];
+    text = [[NSString alloc] initWithData:data 
+                                 encoding:NSASCIIStringEncoding];
+    
+    [self.txtViewGemServerOutput setEditable:YES];
+    [self.txtViewGemServerOutput insertText:text];
+    [self.txtViewGemServerOutput setEditable:NO];
+    
+    if([data length]) {
+        [[notification object] waitForDataInBackgroundAndNotify];
+    }
+    else {
+        [[NSNotificationCenter defaultCenter] removeObserver:self 
+                                                        name:NSFileHandleDataAvailableNotification 
+                                                      object:nil];
+    }
+}
+
+- (IBAction)stopGemServer:(id)sender {
+    [btnStart setEnabled:YES];
+    [btnStop setEnabled:NO];
+    [btnViewDocs setEnabled:NO];
+    [btnClose setEnabled:YES];
+    [txtPort setEditable:YES];
+    
+    NSString *serverText = [NSString stringWithFormat:@"Stopping Gem Server\n"];
+    [self.txtViewGemServerOutput setEditable:YES];
+    [self.txtViewGemServerOutput insertText:serverText];
+    [self.txtViewGemServerOutput setEditable:NO];
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"TrogonStopGemServer" 
+                                                        object:self
+                                                      userInfo:nil];
+}
+
+- (IBAction)viewDocs:(id)sender {
+    NSString *docUrl = [NSString stringWithFormat:@"http://localhost:%@", _port];
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:docUrl]];
+}
+
+- (void)objectSheetDidEnd:(NSWindow *)sheet
+               returnCode:(int)returnCode
+              contextInfo:(void  *)contextInfo {
+    
+    if (returnCode == NSOKButton) {
+    }
+    
+    [objectSheet orderOut:self];
+}
 
 @end
